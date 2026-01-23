c41695e2495911b4b7b7db3d8cac012badd5d254
RawnitGhosh <rawnitghosh2020@dbe-du.org>
2026-01-23 14:07:28 +0530

    hospA

--- data/configs/audit_table_ddl.sql
+++ data/configs/audit_table_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE avd-databricks-demo.temp_dataset.audit_log (
+CREATE TABLE fifth-trainer-484215-c7.temp_dataset.audit_log (
     data_source STRING,
     tablename STRING,
     load_type STRING,
